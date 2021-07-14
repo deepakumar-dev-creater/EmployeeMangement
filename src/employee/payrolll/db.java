012cdcbb2e946eef08d7199f053fe7f2fbfb9990
deepakumar-dev-creater <sean4deepak@gmail.com>
2021-07-14 22:00:15 +0530

    Initial

---
+++ src/employee/payrolll/db.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.payrolll;
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author sean4
+ */
+public class db {
+    Connection conn =null;
+    public static Connection java_db(){
+        
+      try{  
+        Class.forName("org.sqlite.JDBC");
+        Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\sean4\\Desktop\\Employee Payroll System\\db\\mydatabase.sqlite");
+        return conn;
+     }
+      catch(Exception e)
+      {
+        JOptionPane.showMessageDialog(null,e);
+        return null;
+    }
+        
+    }
+    
+    
+}
